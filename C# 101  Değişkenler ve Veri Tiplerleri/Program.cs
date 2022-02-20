cef7d129b102d565a9e35465fb282525d99b6bec
ersoy <mustafaersoymail@gmail.com>
2022-02-20 10:09:13 +0300

    ilk yükleme

---
+++ C# 101  Değişkenler ve Veri Tiplerleri/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int intdeger = -5;
+uint uintdeger=5;
+byte bytedeger=1;
+
+
+Boolean b1=true;
+
+DateTime dt =DateTime.Now;
+
+Console.WriteLine("Lütfen Adınızı Girin");
+
+String name=Console.ReadLine();
+
+Console.WriteLine("Merhaba"+" "+name+"\nSaat :"+DateTime.Now.ToString("hh")+"\nTarih : "+DateTime.Now.ToString("dd:mm:yyyy"));
