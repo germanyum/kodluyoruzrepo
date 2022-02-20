2d8fa21d1683e027fd32be670ef39025879008fc
ersoy <mustafaersoymail@gmail.com>
2022-02-20 11:06:51 +0300

    yukleme

---
+++ C# 101  Operatorler/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+int a = 7;
+int b = 8;
+
+//atama işlemi
+
+a++;
+b--;
+
+a*=a;
+b+=b;
+
+//mantık
+bool b1=true;
+bool b2=false;
+if(b1&&b2)
+    Console.WriteLine("iki koşulda sağlandı");
+if(b1||b2)
+    Console.WriteLine("iki koşuldan biri sağlandı");
+//ilişki
+
+if(a>b)
+    Console.WriteLine("a b den büyük");
+if(b*b>a)
+    Console.WriteLine("b'nin karesi a dan büyük");
+
+// matematik
+int sayi1= 10 ;
+int sayi2 = 11;
+int toplam= sayi1+sayi2;
+
+Console.WriteLine("toplam :" + toplam);
